f0bbfede7d5f4e6cdf5bb4780af8cf55270e3c71
Stefan Norinder <stefan.norinder@sh.se>
2024-10-21 12:42:26 +0200

    Added GuidGenerator

---
+++ OnlinePayment.Logic/Service/GuidGenerator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Logic.Service
+{
+    public class GuidGenerator
+    {
+        public static string GenerateGuidWithoutDashesUppercase()
+        {
+            return Guid.NewGuid().ToString("N").ToUpper();
+        }
+    }
+}
