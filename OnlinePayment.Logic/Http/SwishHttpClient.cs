26672ff8b0144ab9a120aad2e015400d2584f87b
Stefan Norinder <stefan.norinder@sh.se>
2024-10-28 15:31:35 +0100

    removed ceritifate store

--- OnlinePayment.Logic/Http/SwishHttpClient.cs
+++ OnlinePayment.Logic/Http/SwishHttpClient.cs
@@ -27,23 +27,32 @@ namespace OnlinePayment.Logic.Http
 
         protected override async Task<HttpResponseMessage> Send(HttpRequestMessage request)
         {
-            HttpClientHandler handler = GetHttpClientHandlerWithCertificate();
-
-            using (var httpClient = new System.Net.Http.HttpClient(handler))
+            try
             {
-                if (request.RequestUri != null)
-                {
-                    httpClient.BaseAddress = new Uri(request.RequestUri.GetLeftPart(UriPartial.Authority));
-                }
+                HttpClientHandler handler = GetHttpClientHandlerWithCertificate();
 
-                var response = await httpClient.SendAsync(request);
-                if (response.IsSuccessStatusCode) return response;
-                else
+                using (var httpClient = new System.Net.Http.HttpClient(handler))
                 {
-                    string responseContent = await response.Content.ReadAsStringAsync();
-                    string message = $"Error sending request: StatusCode: {response.StatusCode}, Content: {responseContent}";
-                    throw new HttpRequestException(message, null, response.StatusCode);
+                    if (request.RequestUri != null)
+                    {
+                        httpClient.BaseAddress = new Uri(request.RequestUri.GetLeftPart(UriPartial.Authority));
+                    }
+
+                    var response = await httpClient.SendAsync(request);
+                    if (response.IsSuccessStatusCode) return response;
+                    else
+                    {
+                        string responseContent = await response.Content.ReadAsStringAsync();
+                        string message = $"Error sending request: StatusCode: {response.StatusCode}, Content: {responseContent}";
+                        logger.LogInformation(message);
+                        throw new HttpRequestException(message, null, response.StatusCode);
+                    }
                 }
+            }
+            catch (Exception e)
+            {
+                logger.LogError(e, "Error sending request in swish http client");
+                throw;
             }
         }
 
@@ -54,25 +63,19 @@ namespace OnlinePayment.Logic.Http
                 SslProtocols = SslProtocols.Tls12 | SslProtocols.Tls13
             };
 
-            using (var store = new X509Store(StoreName.CertificateAuthority, StoreLocation.CurrentUser))
+            var certs = new X509Certificate2Collection();
+            certs.Import(settings.Certification, settings.Passphrase, X509KeyStorageFlags.DefaultKeySet);
+
+            foreach (var cert in certs)
             {
-                store.Open(OpenFlags.ReadWrite);
-
-                var certs = new X509Certificate2Collection();
-                certs.Import(settings.Certification, settings.Passphrase, X509KeyStorageFlags.DefaultKeySet);
-
-                foreach (var cert in certs)
+                try
                 {
-                    try
-                    {
-                        if (cert.HasPrivateKey) handler.ClientCertificates.Add(cert);
-                        else store.Add(cert);
+                    if (cert.HasPrivateKey) handler.ClientCertificates.Add(cert);
 
-                    }
-                    catch (Exception e)
-                    {
-                        logger.LogError(e, $"Error with certificate: {cert.Subject}. Error: {e.Message}");
-                    }
+                }
+                catch (Exception e)
+                {
+                    logger.LogError(e, $"Error with certificate: {cert.Subject}. Error: {e.Message}");
                 }
             }
             return handler;
